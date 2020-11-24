644f1b0398bcef4994a5cde731b340bd8dcec888
Rostyslav Makhanko <rostyslavm@wix.com>
2020-11-24 04:07:38 +0200

    Add src

---
+++ src/screens/LoginScreen/Component.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {AppRegistry, SafeAreaView, Text} from 'react-native';
+import {Navigation} from 'react-native-navigation';
+
+const Component = () => {
+  return (
+    <SafeAreaView>
+      <Text>Welcome to Uber</Text>
+    </SafeAreaView>
+  );
+};
+
+Navigation.registerComponent('qqq', () => Component);
+
+export default Component;
